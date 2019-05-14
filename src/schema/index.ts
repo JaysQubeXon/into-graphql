214980b6e4d2a54215c7da9ef79e4ecfb8c3d4c5
Noam Gabriel Jacobson <noamgabrieljacobson@Noams-iMac.local>
2019-05-14 19:58:42 -0400

    Schema Definition. Query, Mutation, Event, Participant and Error types added. Added scalar Time for timestamp type.

---
+++ src/schema/index.ts
@@ -0,0 +1,40 @@
+import { gql } from "apollo-server-express";
+
+export default gql`
+  scalar Time
+
+  type Query {
+    getEventByID(id: ID!): Event
+  }
+  type Mutation {
+    addEvent(
+      start: Time!
+      end: Time
+      title: String!
+      description: String
+      participants: [ID]
+    ): Event
+  }
+
+  type Event {
+    id: ID
+    start: Time
+    end: Time
+    title: String
+    description: String
+    participants: [Participant]
+    errors: [Error]
+  }
+
+  type Participant {
+    id: ID
+    name: String
+    events: [Event]
+    errors: [Error]
+  }
+
+  type Error {
+    error: String
+    message: String
+  }
+`;
